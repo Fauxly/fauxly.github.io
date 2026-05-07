00380531d745c05ddf24adde31d345dd06b871db
Fauxly <tailormoon@icloud.com>
2026-05-07 14:19:06 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,37 @@
+#!/bin/bash
+
+echo "Updating Packages..."
+
 dpkg-scanpackages -m ./debs > Packages
-gzip -c Packages > Packages.gz
 
-cd beta
+gzip -kf Packages
 
-dpkg-scanpackages -m ./debs > Packages
-gzip -c Packages > Packages.gz
+echo "Generating Release file..."
 
-cd ..
+cat > Release << EOF
+Origin: Fauxly Repo
+Label: Fauxly Repo
+Suite: stable
+Version: 1.0
+Codename: tvos
+Architectures: iphoneos-arm64
+Components: main
+Description: Fauxly tvOS Jailbreak Repository
+EOF
+
+echo "Generating SHA256 hashes..."
+
+SHA256_PACKAGES=$(shasum -a 256 Packages | awk '{ print $1 }')
+SHA256_GZ=$(shasum -a 256 Packages.gz | awk '{ print $1 }')
+
+SIZE_PACKAGES=$(stat -f%z Packages)
+SIZE_GZ=$(stat -f%z Packages.gz)
+
+cat >> Release << EOF
+
+SHA256:
+ $SHA256_PACKAGES $SIZE_PACKAGES Packages
+ $SHA256_GZ $SIZE_GZ Packages.gz
+EOF
+
+echo "Repository updated successfully."
